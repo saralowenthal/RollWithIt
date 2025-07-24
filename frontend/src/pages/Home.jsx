045471843ae2bf2bb841eed5d2db7e26e02460af
Pesia Mittelman <pessie.mittelman@gmail.com>
2025-07-24 15:09:41 -0400

    adding env file for amplify

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,9 +9,9 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [addingNew, setAddingNew] = useState(false);
   const [newName, setNewName] = useState('');
-
-  const BASE_API_URL = import.meta.env.VITE_API_BASE_URL;
   
+  const BASE_API_URL = import.meta.env.VITE_API_BASE_URL;
+
   useEffect(() => {
     const fetchLists = async () => {
       try {
