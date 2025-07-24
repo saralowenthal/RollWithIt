5581d31652caebf8546de3a63266373cbef2c2f3
Pesia Mittelman <pessie.mittelman@gmail.com>
2025-07-24 15:05:30 -0400

    adding amplify to env file

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -11,7 +11,7 @@ function List() {
   const [newItem, setNewItem] = useState('');
   const [loading, setLoading] = useState(true);
 
-  const BASE_API_URL = 'https://h2fqo38sa8.execute-api.us-east-1.amazonaws.com';
+  const BASE_API_URL = import.meta.env.VITE_API_BASE_URL;
 
   const fetchList = async () => {
     try {
